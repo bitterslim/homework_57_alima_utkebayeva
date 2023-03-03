f49a0d4c92317fb64ca8e1390931ef6843bd9a67
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-03 18:33:17 +0600

    Добвлены ссылки на страницы

--- webapp/urls.py
+++ webapp/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('task/<int:pk>', TaskDetailView.as_view(), name = 'task_detail'),
     path('task/<int:pk>/edit', TaskUpdateView.as_view(), name = 'task_update'),
     path('task/<int:pk>/delete', TaskDeleteView.as_view(), name='task_delete')
-
 ]
