0d297f92ced8fea033150238483c6c860eee1338
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-03 18:28:55 +0600

    Создано классовое представление IndexView

--- webapp/views/base.py
+++ webapp/views/base.py
@@ -1,6 +1,6 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from webapp.models import Task
 
@@ -13,9 +13,5 @@ class IndexView(TemplateView):
         context['task'] = Task.objects.all()
         return context
 
-# def index_view(request: WSGIRequest):
-#     task = Task.objects.all()
-#     context = {
-#         'task' : task
-#     }
-#     return render(request, 'index.html', context=context)
+class IndexRedirectView(RedirectView):
+    pattern_name = 'index'
